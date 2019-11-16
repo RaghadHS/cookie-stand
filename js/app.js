738da41d2611e04ea56db881890bd937497e416d
RaghadHS <raghad.alsawalhi@gmail.com>
2019-11-16 23:14:06 +0200

    First trial

--- js/app.js
+++ js/app.js
@@ -8,7 +8,12 @@ var Seattle = {
   location: 'Seattle',
   minCust: 23,
   maxCust: 65,
-  avgSale:6.3,
+  avgSalePC:6.3,
+
+  randomCust : function( minCust, maxCust){
+
+    return Math.ceil(Math.random() * ((maxCust - minCust) + 1)) + minCust;
+  },
 };
 
 ///Creating object 2
@@ -17,7 +22,12 @@ var Tokyo = {
   location: 'Tokyo',
   minCust: 3,
   maxCust: 24,
-  avgSale: 1.2,
+  avgSalePC: 1.2,
+
+  randomCust : function( minCust, maxCust){
+
+    return Math.ceil(Math.random() * ((maxCust - minCust) + 1)) + minCust;
+  },
 };
 
 
@@ -27,35 +37,211 @@ var Dubai = {
   location: 'Dubai',
   minCust: 11,
   maxCust: 38,
-  avgSale: 3.7,
+  avgSalePC: 3.7,
+
+  randomCust : function( minCust, maxCust){
+
+    return Math.ceil(Math.random() * ((maxCust - minCust) + 1)) + minCust;
+  },
 };
 
 
-///Creating object 3
+///Creating object 4
 var Paris = {
 
   location: 'Paris',
   minCust: 20,
   maxCust: 38,
-  avgSale: 2.3,
+  avgSalePC: 2.3,
+
+  randomCust : function( minCust, maxCust){
+
+    return Math.ceil(Math.random() * ((maxCust - minCust) + 1)) + minCust;
+  },
 };
 
-///Creating object 3
+///Creating object 5
 var Lima = {
 
   location: 'Lima',
   minCust: 2,
   maxCust: 16,
-  avgSale: 4.6,
+  avgSalePC: 4.6,
+
+  randomCust : function( minCust, maxCust){
+
+    return Math.ceil(Math.random() * ((maxCust - minCust) + 1)) + minCust;
+  },
+
 };
 
-function hours(){
+///Adding name of Seattle
+function infoSeattle(){
+  var paragraph = document.createElement('p');
+  var content = document.getElementById('content');
+  content.appendChild(paragraph);
 
-  for( var i = 0; i < 5; i++)
+  var ulList = document.createElement('ul');
+  ulList.textContent = Seattle.location;
+  content.appendChild(ulList);
+  var total = document.createElement('li');
+  var value = 0;
+
+
+  for (var i = 6; i < 13; i++ )
   {
-    document.appendChild('List number : ' + i);
-    
+    var liHOURSALE = document.createElement('li');
+    value = Seattle.randomCust(Seattle.minCust, Seattle.maxCust);
+    liHOURSALE.textContent = i + ' AM: ' + value + ' Cookies';
+    total.textContent += value;
+    ulList.appendChild(liHOURSALE);
+  }
+  for (var y = 1; y < 8; y++ )
+  {
+    liHOURSALE = document.createElement('li');
+    value = Seattle.randomCust(Seattle.minCust, Seattle.maxCust);
+    liHOURSALE.textContent = y + ' PM: ' + value + ' Cookies' ;
+    ulList.appendChild(liHOURSALE);
+    total.textContent += value;
   }
 
+  ulList.appendChild(total);
 }
-hours();
+
+infoSeattle();
+
+///Adding name of Tokyo
+function infoTokyo(){
+  var paragraph = document.createElement('p');
+  var content = document.getElementById('content');
+  content.appendChild(paragraph);
+
+  var ulList = document.createElement('ul');
+  ulList.textContent = Tokyo.location;
+  content.appendChild(ulList);
+  var total = document.createElement('li');
+  var value = 0;
+  
+  
+  for (var i = 6; i < 13; i++ )
+  {
+    var liHOURSALE = document.createElement('li');
+    value = Tokyo.randomCust(Tokyo.minCust, Tokyo.maxCust);
+    liHOURSALE.textContent = i + ' AM: ' + value + ' Cookies';
+    total.textContent += value;
+    ulList.appendChild(liHOURSALE);
+  }
+  for (var y = 1; y < 8; y++ )
+  {
+    liHOURSALE = document.createElement('li');
+    value = Tokyo.randomCust(Tokyo.minCust, Tokyo.maxCust);
+    liHOURSALE.textContent = y + ' PM: ' + value + ' Cookies' ;
+    ulList.appendChild(liHOURSALE);
+    total.textContent += value;
+  }
+  
+  ulList.appendChild(total);
+}
+infoTokyo();
+
+///Adding name of Dubai
+function infoDubai(){
+  var paragraph = document.createElement('p');
+  var content = document.getElementById('content');
+  content.appendChild(paragraph);
+  
+  var ulList = document.createElement('ul');
+  ulList.textContent = Dubai.location;
+  content.appendChild(ulList);
+  var total = document.createElement('li');
+  var value = 0;
+    
+    
+  for (var i = 6; i < 13; i++ )
+  {
+    var liHOURSALE = document.createElement('li');
+    value = Dubai.randomCust(Dubai.minCust, Dubai.maxCust);
+    liHOURSALE.textContent = i + ' AM: ' + value + ' Cookies';
+    total.textContent += value;
+    ulList.appendChild(liHOURSALE);
+  }
+  for (var y = 1; y < 8; y++ )
+  {
+    liHOURSALE = document.createElement('li');
+    value = Dubai.randomCust(Dubai.minCust, Dubai.maxCust);
+    liHOURSALE.textContent = y + ' PM: ' + value + ' Cookies' ;
+    ulList.appendChild(liHOURSALE);
+    total.textContent += value;
+  }
+    
+  ulList.appendChild(total);
+}
+infoDubai();
+
+///Adding name of Paris
+function infoParis(){
+  var paragraph = document.createElement('p');
+  var content = document.getElementById('content');
+  content.appendChild(paragraph);
+  
+  var ulList = document.createElement('ul');
+  ulList.textContent = Paris.location;
+  content.appendChild(ulList);
+  var total = document.createElement('li');
+  var value = 0;
+    
+    
+  for (var i = 6; i < 13; i++ )
+  {
+    var liHOURSALE = document.createElement('li');
+    value = Paris.randomCust(Paris.minCust, Paris.maxCust);
+    liHOURSALE.textContent = i + ' AM: ' + value + ' Cookies';
+    total.textContent += value;
+    ulList.appendChild(liHOURSALE);
+  }
+  for (var y = 1; y < 8; y++ )
+  {
+    liHOURSALE = document.createElement('li');
+    value = Paris.randomCust(Paris.minCust, Paris.maxCust);
+    liHOURSALE.textContent = y + ' PM: ' + value + ' Cookies' ;
+    ulList.appendChild(liHOURSALE);
+    total.textContent += value;
+  }
+    
+  ulList.appendChild(total);
+}
+infoParis();
+
+///Adding name of Lima
+function infoLima(){
+  var paragraph = document.createElement('p');
+  var content = document.getElementById('content');
+  content.appendChild(paragraph);
+    
+  var ulList = document.createElement('ul');
+  ulList.textContent = Lima.location;
+  content.appendChild(ulList);
+  var total = document.createElement('li');
+  var value = 0;
+      
+      
+  for (var i = 6; i < 13; i++ )
+  {
+    var liHOURSALE = document.createElement('li');
+    value = Lima.randomCust(Lima.minCust, Lima.maxCust);
+    liHOURSALE.textContent = i + ' AM: ' + value + ' Cookies';
+    total.textContent += value;
+    ulList.appendChild(liHOURSALE);
+  }
+  for (var y = 1; y < 8; y++ )
+  {
+    liHOURSALE = document.createElement('li');
+    value = Lima.randomCust(Lima.minCust, Lima.maxCust);
+    liHOURSALE.textContent = y + ' PM: ' + value + ' Cookies' ;
+    ulList.appendChild(liHOURSALE);
+    total.textContent += value;
+  }
+      
+  ulList.appendChild(total);
+}
+infoLima();
