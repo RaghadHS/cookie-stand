e5ff84b498f9174d8ebad7865b7864637926c9fe
RaghadHS <raghad.alsawalhi@gmail.com>
2019-11-18 14:08:30 +0200

    Tables

--- js/app.js
+++ js/app.js
@@ -3,255 +3,219 @@ var summation = 0;
 //Creating object literals
 
 ///Creating object 1
-var Seattle = {
+var seattle = {
 
   location: 'Seattle',
   minCust: 23,
   maxCust: 65,
   avgSalePC:6.3,
-
+  arrayShop: [],
   randomCust : function( minCust, maxCust){
 
     return Math.ceil(Math.random() * ((maxCust - minCust) + 1)) + minCust;
   },
+  info : function()
+  {
+    var value = 0;
+
+    for (var i = 6; i < 13; i++ )
+    {
+      //var liHOURSALE = document.createElement('li');
+      value = this.randomCust(this.minCust, this.maxCust);
+      this.arrayShop[i - 6] = value; 
+      summation = summation + value;
+    
+    }
+    for ( i = 13; i < 21; i++ )
+    {
+
+      value = this.randomCust(this.minCust, this.maxCust);
+      this.arrayShop[i - 6] = value;
+      summation = summation + value;
+    }
+
+    if( i === 21)
+    {
+      this.arrayShop[i - 6] = summation;
+    }
+  },
 };
 
 ///Creating object 2
-var Tokyo = {
+var tokyo = {
 
   location: 'Tokyo',
   minCust: 3,
   maxCust: 24,
   avgSalePC: 1.2,
+  arrayShop: [],
 
   randomCust : function( minCust, maxCust){
 
     return Math.ceil(Math.random() * ((maxCust - minCust) + 1)) + minCust;
   },
+  info : function()
+  {
+    var value = 0;
+
+    for (var i = 6; i < 13; i++ )
+    {
+      //var liHOURSALE = document.createElement('li');
+      value = this.randomCust(this.minCust, this.maxCust);
+      this.arrayShop[i - 6] = value; 
+      summation = summation + value;
+    
+    }
+    for ( i = 13; i < 21; i++ )
+    {
+
+      value = this.randomCust(this.minCust, this.maxCust);
+      this.arrayShop[i - 6] = value;
+      summation = summation + value;
+    }
+
+    if (i === 21 )
+    {
+      this.arrayShop[i - 6] = summation;
+    }
+
+  },
 };
 
 
-///Creating object 3
-var Dubai = {
+
+
+// ///Creating object 3
+var dubai = {
 
   location: 'Dubai',
   minCust: 11,
   maxCust: 38,
   avgSalePC: 3.7,
+  arrayShop: [],
 
   randomCust : function( minCust, maxCust){
 
     return Math.ceil(Math.random() * ((maxCust - minCust) + 1)) + minCust;
+  },
+  info : function()
+  {
+    var value = 0;
+
+    for (var i = 6; i < 13; i++ )
+    {
+      //var liHOURSALE = document.createElement('li');
+      value = this.randomCust(this.minCust, this.maxCust);
+      this.arrayShop[i - 6] = value; 
+      summation = summation + value;
+    
+    }
+    for ( i = 13; i < 21; i++ )
+    {
+
+      value = this.randomCust(this.minCust, this.maxCust);
+      this.arrayShop[i - 6] = value;
+      summation = summation + value;
+    }
+
+    if (i === 21)
+    {
+      this.arrayShop[i - 6] = summation;
+       
+    }
+
   },
 };
 
 
 ///Creating object 4
-var Paris = {
+var paris = {
 
   location: 'Paris',
   minCust: 20,
   maxCust: 38,
   avgSalePC: 2.3,
+  arrayShop: [],
 
   randomCust : function( minCust, maxCust){
 
     return Math.ceil(Math.random() * ((maxCust - minCust) + 1)) + minCust;
   },
+  info : function()
+  {
+    var value = 0;
+
+    for (var i = 6; i < 13; i++ )
+    {
+      //var liHOURSALE = document.createElement('li');
+      value = this.randomCust(this.minCust, this.maxCust);
+      this.arrayShop[i - 6] = value; 
+      summation = summation + value;
+    
+    }
+    for ( i = 13; i < 21; i++ )
+    {
+
+      value = this.randomCust(this.minCust, this.maxCust);
+      this.arrayShop[i - 6] = value;
+      summation = summation + value;
+    }
+
+    if (i === 21)
+    {
+      this.arrayShop[i - 6] = summation;
+    }
+  },
 };
 
 ///Creating object 5
-var Lima = {
+var lima = {
 
   location: 'Lima',
   minCust: 2,
   maxCust: 16,
   avgSalePC: 4.6,
+  arrayShop: [],
 
   randomCust : function( minCust, maxCust){
 
     return Math.ceil(Math.random() * ((maxCust - minCust) + 1)) + minCust;
   },
 
+  info : function()
+  {
+    var value = 0;
+
+    for (var i = 6; i < 13; i++ )
+    {
+      //var liHOURSALE = document.createElement('li');
+      value = this.randomCust(this.minCust, this.maxCust);
+      this.arrayShop[i - 6] = value; 
+      summation = summation + value;
+    
+    }
+    for ( i = 13; i < 21; i++ )
+    {
+
+      value = this.randomCust(this.minCust, this.maxCust);
+      this.arrayShop[i - 6] = value;
+      summation = summation + value;
+    }
+
+    if ( i === 21)
+    {
+      this.arrayShop[i - 6] = summation;
+    }
+  },
+
 };
 
-///Adding name of Seattle
-function infoSeattle(){
-  var paragraph = document.createElement('p');
-  var content = document.getElementById('content');
-  content.appendChild(paragraph);
+seattle.info();
+tokyo.info();
+dubai.info();
+paris.info();
+lima.info();
 
-  var ulList = document.createElement('ul');
-  ulList.textContent = Seattle.location;
-  content.appendChild(ulList);
-  var total = document.createElement('li');
-  var value = 0;
-
-
-  for (var i = 6; i < 13; i++ )
-  {
-    var liHOURSALE = document.createElement('li');
-    value = Seattle.randomCust(Seattle.minCust, Seattle.maxCust);
-    liHOURSALE.textContent = i + ' AM: ' + value + ' Cookies';
-    summation = summation + value;
-    ulList.appendChild(liHOURSALE);
-  }
-  for (var y = 1; y < 8; y++ )
-  {
-    liHOURSALE = document.createElement('li');
-    value = Seattle.randomCust(Seattle.minCust, Seattle.maxCust);
-    liHOURSALE.textContent = y + ' PM: ' + value + ' Cookies' ;
-    ulList.appendChild(liHOURSALE);
-    summation = summation + value;
-  }
-  total.textContent = summation; 
-  ulList.appendChild(total);
- 
-}
-
-  
-infoSeattle();
-
-///Adding name of Tokyo
-function infoTokyo(){
-  var paragraph = document.createElement('p');
-  var content = document.getElementById('content');
-  content.appendChild(paragraph);
-
-  var ulList = document.createElement('ul');
-  ulList.textContent = Tokyo.location;
-  content.appendChild(ulList);
-  var total = document.createElement('li');
-  var value = 0;
-  
-  
-  for (var i = 6; i < 13; i++ )
-  {
-    
-    var liHOURSALE = document.createElement('li');
-    value = Tokyo.randomCust(Tokyo.minCust, Tokyo.maxCust);
-    liHOURSALE.textContent = i + ' AM: ' + value + ' Cookies';
-    summation = summation + value;
-    ulList.appendChild(liHOURSALE);
-  }
-  
-  for (var y = 1; y < 8; y++ )
-  {
-    liHOURSALE = document.createElement('li');
-    value = Tokyo.randomCust(Tokyo.minCust, Tokyo.maxCust);
-    liHOURSALE.textContent = y + ' PM: ' + value + ' Cookies' ;
-    summation = summation + value;
-    ulList.appendChild(liHOURSALE);
-  }
-  total.textContent = summation;
-  
-  ulList.appendChild(total);
-}
-infoTokyo();
-
-///Adding name of Dubai
-function infoDubai(){
-  var paragraph = document.createElement('p');
-  var content = document.getElementById('content');
-  content.appendChild(paragraph);
-  
-  var ulList = document.createElement('ul');
-  ulList.textContent = Dubai.location;
-  content.appendChild(ulList);
-  var total = document.createElement('li');
-  var value = 0;
-    
-    
-  for (var i = 6; i < 13; i++ )
-  {
-    var liHOURSALE = document.createElement('li');
-    value = Dubai.randomCust(Dubai.minCust, Dubai.maxCust);
-    liHOURSALE.textContent = i + ' AM: ' + value + ' Cookies';
-    total.textContent += value;
-    ulList.appendChild(liHOURSALE);
-  }
-  for (var y = 1; y < 8; y++ )
-  {
-    liHOURSALE = document.createElement('li');
-    value = Dubai.randomCust(Dubai.minCust, Dubai.maxCust);
-    liHOURSALE.textContent = y + ' PM: ' + value + ' Cookies' ;
-    ulList.appendChild(liHOURSALE);
-    total.textContent += value;
-  }
-    
-  ulList.appendChild(total);
-}
-infoDubai();
-
-///Adding name of Paris
-function infoParis(){
-  var paragraph = document.createElement('p');
-  var content = document.getElementById('content');
-  content.appendChild(paragraph);
-  
-  var ulList = document.createElement('ul');
-  ulList.textContent = Paris.location;
-  content.appendChild(ulList);
-  var total = document.createElement('li');
-  var value = 0;
-    
-    
-  for (var i = 6; i < 13; i++ )
-  {
-    var liHOURSALE = document.createElement('li');
-    value = Paris.randomCust(Paris.minCust, Paris.maxCust);
-    liHOURSALE.textContent = i + ' AM: ' + value + ' Cookies';
-    total.textContent += value;
-    ulList.appendChild(liHOURSALE);
-  }
-  for (var y = 1; y < 8; y++ )
-  {
-    liHOURSALE = document.createElement('li');
-    value = Paris.randomCust(Paris.minCust, Paris.maxCust);
-    liHOURSALE.textContent = y + ' PM: ' + value + ' Cookies' ;
-    ulList.appendChild(liHOURSALE);
-    total.textContent += value;
-  }
-    
-  ulList.appendChild(total);
-}
-infoParis();
-
-///Adding name of Lima
-function infoLima(){
-  var paragraph = document.createElement('p');
-  var content = document.getElementById('content');
-  content.appendChild(paragraph);
-    
-  var ulList = document.createElement('ul');
-  ulList.textContent = Lima.location;
-  content.appendChild(ulList);
-  var total = document.createElement('li');
-  var value = 0;
-      
-      
-  for (var i = 6; i < 13; i++ )
-  {
-    var liHOURSALE = document.createElement('li');
-    value = Lima.randomCust(Lima.minCust, Lima.maxCust);
-    liHOURSALE.textContent = i + ' AM: ' + value + ' Cookies';
-    total.textContent += value;
-    ulList.appendChild(liHOURSALE);
-  }
-  for (var y = 1; y < 8; y++ )
-  {
-    liHOURSALE = document.createElement('li');
-    value = Lima.randomCust(Lima.minCust, Lima.maxCust);
-    liHOURSALE.textContent = y + ' PM: ' + value + ' Cookies' ;
-    ulList.appendChild(liHOURSALE);
-    total.textContent += value;
-  }
-      
-  ulList.appendChild(total);
-}
-infoLima();
-
-////////////END OF FUNCTIONS////////////
 
 /////Creation of table
 var tables = document.createElement('table');
@@ -260,13 +224,15 @@ tableLocation.appendChild(tables);
 var heading = document.createElement('th');
 heading.textContent = '  ';
 tables.appendChild(heading);
+
 ////Up till here, we created the table and added an empty column 
 ///to shift everything one cell to the right
 
 
 
 ///Function to add time header to columns
-for( var headingCounter = 6 ; headingCounter < 20; headingCounter++){
+for( var headingCounter = 6 ; headingCounter < 21; headingCounter++){
+
   heading = document.createElement('th');
   heading.textContent = headingCounter + 'AM';
   if(headingCounter === 12)
@@ -277,13 +243,46 @@ for( var headingCounter = 6 ; headingCounter < 20; headingCounter++){
   {
     heading.textContent = (headingCounter - 12) + 'PM';
   }
+//// CONTINUE HERE
+  if (headingCounter === 21)
+  {
+    heading.textContent = 'DAILY TOTAL';
+  }
   heading.width = '70px';
   tables.appendChild(heading);
 }
 
 ////Adding the rows to the table
 var rowHeading = document.createElement('tr');
-rowHeading.textContent = 'First Row';
+rowHeading.textContent = '';
+var colMargin = document.createElement('td');
 rowHeading.width = '30px';
 tables.appendChild(rowHeading);
+rowHeading.appendChild(colMargin);
 
+
+////Function to add arrayValue in table
+
+function addArrToTable (location, array){
+  var newRow = document.createElement('tr');
+  tables.appendChild(newRow);
+  var cityName = document.createElement('td');
+  cityName.textContent = location;
+  newRow.appendChild(cityName);
+  for ( var c = 0; c < 15; c++)
+  {
+
+    var columnForShop = document.createElement('td');
+    columnForShop.textContent = array[c];
+    newRow.appendChild(columnForShop);
+
+  }
+// columnForShop = document.createElement('td');
+// columnForShop.textContent
+}
+
+addArrToTable(seattle.location, seattle.arrayShop);
+addArrToTable(tokyo.location, tokyo.arrayShop);
+addArrToTable(dubai.location, dubai.arrayShop);
+addArrToTable(paris.location, paris.arrayShop);
+addArrToTable(lima.location, lima.arrayShop);
