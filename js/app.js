faf32540ace86b5d2f11a6a76dff97e16858f7ea
RaghadHS <raghad.alsawalhi@gmail.com>
2019-11-19 10:43:21 +0200

    Adding tables by constructor methods

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 var summation = 0;
-
+var hold = [ ];
+var tables = document.createElement('table');
 ///////CONSTRUCTOR///////////
 function Store (location, minCust, maxCust, avgSalePC, arrayShop)
 {
@@ -11,8 +12,13 @@ function Store (location, minCust, maxCust, avgSalePC, arrayShop)
   this.avgSalePC = avgSalePC;
   this.arrayShop = arrayShop;
 
+  this.randomCust(this.minCust, this.maxCust);
+  hold = this.info();
+  this.arrayShop = hold;
+  this.addArrToTable(this.location, this.arrayShop);
 }
 
+alert(hold);
 /////////END OF CONSTRUCTOR////////////////
 Store.prototype.randomCust = function(minCust, maxCust)
 {
@@ -31,7 +37,6 @@ Store.prototype.info = function()
     value = this.randomCust(this.minCust, this.maxCust);
     this.arrayShop[i - 6] = value; 
     summation = summation + value;
-    
   }
   for ( i = 13; i < 21; i++ )
   {
@@ -45,6 +50,9 @@ Store.prototype.info = function()
   {
     this.arrayShop[i - 6] = summation;
   }
+
+  return this.arrayShop;
+  
 };
 
 Store.prototype.addArrToTable = function (location, arrayShop) {
@@ -54,119 +62,103 @@ Store.prototype.addArrToTable = function (location, arrayShop) {
   var cityName = document.createElement('td');
   cityName.textContent = location;
   newRow.appendChild(cityName);
-  for ( var c = 0; c < 15; c++)
+  for ( var c = 0; c < 14; c++)
   {
-  
     var columnForShop = document.createElement('td');
     columnForShop.textContent = arrayShop[c];
     newRow.appendChild(columnForShop);
-  
   }
-  // columnForShop = document.createElement('td');
-  // columnForShop.textContent
+  columnForShop = document.createElement('td');
+  ///columnForShop.textContent
 };
 
+/////Creation of table
+function createTable(){
+  // var tables = document.createElement('table');
+  var tableLocation = document.getElementById('content');
+  // tableLocation.appendChild(tables);
+  
+  var heading = document.createElement('th');
+  heading.textContent = '  ';
+  tables.appendChild(heading);
+  tableLocation.appendChild(tables);
+
+  ///Add time header to columns
+  for( var headingCounter = 6 ; headingCounter < 21; headingCounter++){
+
+    heading = document.createElement('th');
+    if (headingCounter === 21)
+    {
+      heading.textContent = 'DAILY TOTAL';
+    }
+    heading.textContent = headingCounter + 'AM';
+    if(headingCounter === 12)
+    {
+      heading.textContent = headingCounter + 'PM';
+    }
+    if (headingCounter > 12)
+    {
+      heading.textContent = (headingCounter - 12) + 'PM';
+    }
+    //// CONTINUE HERE
+    // if (headingCounter === 21)
+    // {
+    //   heading.textContent = 'DAILY TOTAL';
+    // }
+    heading.width = '70px';
+    tables.appendChild(heading);
+  }
 
 
-
-
-
+  var rowHeading = document.createElement('tr');
+  rowHeading.textContent = '';
+  var colMargin = document.createElement('td');
+  rowHeading.width = '30px';
+  tables.appendChild(rowHeading);
+  rowHeading.appendChild(colMargin);
+}
 //Creating object literals
+
+createTable();
 
 ///Creating object 1
 var contentArr = [];
 var seattle = new Store('Seattle', 23 , 65 , 6.3, contentArr);
+
+
 ///Creating object 2
 var contentArr2 = [];
 var tokyo = new Store('Tokyo', 3 , 24 , 1.2, contentArr2);
+
 ///Creating object 3
 var contentArr3 = [];
 var dubai = new Store('Dubai', 11, 38, 3.7, contentArr3);
+
 ///Creating object 4
 var contentArr4 = [];
 var paris = new Store('Paris', 20, 38, 2.3, contentArr4);
+
 ///Creating object 5
 var contentArr5 = [];
 var lima = new Store('Lima', 2, 16, 4.6, contentArr5);
 
   
-seattle.info();
-tokyo.info();
-dubai.info();
-paris.info();
-lima.info();
+// seattle.info();
+// tokyo.info();
+// dubai.info();
+// paris.info();
+// lima.info();
 
-
-/////Creation of table
-var tables = document.createElement('table');
-var tableLocation = document.getElementById('content');
-tableLocation.appendChild(tables);
-var heading = document.createElement('th');
-heading.textContent = '  ';
-tables.appendChild(heading);
-
-////Up till here, we created the table and added an empty column 
-///to shift everything one cell to the right
-
-
-
-///Function to add time header to columns
-for( var headingCounter = 6 ; headingCounter < 21; headingCounter++){
-
-  heading = document.createElement('th');
-  if (headingCounter === 21)
-  {
-    heading.textContent = 'DAILY TOTAL';
-  }
-  heading.textContent = headingCounter + 'AM';
-  if(headingCounter === 12)
-  {
-    heading.textContent = headingCounter + 'PM';
-  }
-  if (headingCounter > 12)
-  {
-    heading.textContent = (headingCounter - 12) + 'PM';
-  }
-  //// CONTINUE HERE
-  // if (headingCounter === 21)
-  // {
-  //   heading.textContent = 'DAILY TOTAL';
-  // }
-  heading.width = '70px';
-  tables.appendChild(heading);
-}
-
-////Adding the rows to the table
-var rowHeading = document.createElement('tr');
-rowHeading.textContent = '';
-var colMargin = document.createElement('td');
-rowHeading.width = '30px';
-tables.appendChild(rowHeading);
-rowHeading.appendChild(colMargin);
 
 
 ////Function to add arrayValue in table
 
-function addArrToTable (location, array){
-  var newRow = document.createElement('tr');
-  tables.appendChild(newRow);
-  var cityName = document.createElement('td');
-  cityName.textContent = location;
-  newRow.appendChild(cityName);
-  for ( var c = 0; c < 15; c++)
-  {
+////////////////OLD PLACE OF ADDTOSTORE FUNCTION///////////////////
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 
-    var columnForShop = document.createElement('td');
-    columnForShop.textContent = array[c];
-    newRow.appendChild(columnForShop);
-
-  }
-// columnForShop = document.createElement('td');
-// columnForShop.textContent
-}
-
-addArrToTable(seattle.location, seattle.arrayShop);
-addArrToTable(tokyo.location, tokyo.arrayShop);
-addArrToTable(dubai.location, dubai.arrayShop);
-addArrToTable(paris.location, paris.arrayShop);
-addArrToTable(lima.location, lima.arrayShop);
+//addArrToTable(seattle.location, seattle.arrayShop);
+// addArrToTable(tokyo.location, tokyo.arrayShop);
+// addArrToTable(dubai.location, dubai.arrayShop);
+// addArrToTable(paris.location, paris.arrayShop);
+// addArrToTable(lima.location, lima.arrayShop);
